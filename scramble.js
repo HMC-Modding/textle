6ec5417a6286a42c1475f1360da05ca4616f08f2
HMC-Modding <152757396+HMC-Modding@users.noreply.github.com>
2024-02-23 11:34:58 -0500

    Create scramble.js

---
+++ scramble.js
@@ -0,0 +1,35 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+var list, index;
+list = document.getElementsByClassName("scramble");
+for (index = 0; index < list.length; ++index) {
+  list[index].setAttribute("onmouseover", "scramble(this)");
+  list[index].setAttribute("data-value", list[index].innerText);
+}
+
+function scramble(x) {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    x.innerText = x.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return x.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= x.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 2;
+  }, 30);
+}
